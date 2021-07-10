eef8909813c512f70c311c311572d991c2152f0c
Karanbisht511 <karanbisht511@gmai.com>
2021-07-10 18:18:29 +0530

    notes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,4 +1,3 @@
-import { Hidden } from "@material-ui/core";
 import React, { useState } from "react";
 import "./Notes.css";
 
@@ -6,11 +5,18 @@ function Notes() {
   const takeNote = "take note";
   const closeNote = "close note";
   const [noteOpen, setNoteOpen] = useState(false);
+  const [currNote, setCurrNote] = useState("hello world"); //to store notes in backend/database we can use currNote
 
   function handleClick() {
     setNoteOpen(() => {
       return !noteOpen;
     });
+  }
+
+  function handleChange(event) {
+    // console.log(event.target.value);
+    setCurrNote(event.target.value);
+    console.log(currNote);
   }
 
   return (
@@ -21,10 +27,12 @@ function Notes() {
       </button>
 
       <input
+        onChange={handleChange}
         type="textArea"
         className="textArea"
         style={noteOpen ? { visibility: "visible" } : { visibility: "hidden" }}
         placeholder="Take notes here:"
+        value={currNote}
       ></input>
     </div>
   );
